6b306a81ad44edc13ba98d173c4e98e7501cb712
Matt Jadud <matthew.jadud@gsa.gov>
2023-08-09 02:20:15 -0400

    Adding an example section
    This draws from the instructions that are part of the FAC app repo and
    the Excel document definition.
    
    It serves as an example of what we could do.

---
+++ fetch_files.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+templates=("additional-ueis-workbook.json"
+	"audit-findings-text-workbook.json"
+	"corrective-action-plan-workbook.json"	
+	"federal-awards-audit-findings-workbook.json"	
+	"federal-awards-workbook.json"
+	"notes-to-sefa-workbook.json"
+	"secondary-auditors-workbook.json"
+	) 
+
+schemas=(
+	
+)
+
+instructions=(
+	"InstructionsSecondaryAuditors.libsonnet"
+)
+
+git clone --branch lh/workbook-instructions https://github.com/GSA-TTS/FAC wbi
+
+
+for file in ${templates[@]};
+do
+	cp wbi/backend/schemas/output/excel/json/${file} src/_data/workbooks/${file}
+done
+
+for file in ${instructions[@]};
+do
+	cp wbi/backend/schemas/source/excel/libs/${file} src/_data/workbooks/${file}
+	base=$(basename "${file}" .libsonnet)
+	jsonnet src/_data/workbooks/${file} > "src/_data/workbooks/${base}.json"
+done
+
+rm -rf wbi
