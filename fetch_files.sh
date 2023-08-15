dcd58701d8f56e86ee0c0254e2ac02f73fb959c3
Matt Henry <hey@matthenry.fyi>
2023-08-15 14:17:10 -0400

    Add additional instruction data

--- fetch_files.sh
+++ fetch_files.sh
@@ -1,35 +1,40 @@
 #!/bin/bash
 
-templates=("additional-ueis-workbook.json"
+templates=(
+	"additional-ueis-workbook.json"
 	"audit-findings-text-workbook.json"
-	"corrective-action-plan-workbook.json"	
-	"federal-awards-audit-findings-workbook.json"	
+	"corrective-action-plan-workbook.json"
+	"federal-awards-audit-findings-workbook.json"
 	"federal-awards-workbook.json"
 	"notes-to-sefa-workbook.json"
 	"secondary-auditors-workbook.json"
-	) 
+)
 
 schemas=(
-	
+
 )
 
 instructions=(
+	"InstructionsSecondaryAuditors.libsonnet"
+	"InstructionsFederalAwardsFindings.libsonnet"
+	"InstructionsAdditionalUEIs.libsonnet"
+	"InstructionsCorrectiveActionPlan.libsonnet"
+	"InstructionsFederalAwards.libsonnet"
+	"InstructionsFederalAwardsAuditFindingsText.libsonnet"
+	"InstructionsFederalAwardsFindings.libsonnet"
 	"InstructionsSecondaryAuditors.libsonnet"
 )
 
 git clone --branch lh/workbook-instructions https://github.com/GSA-TTS/FAC wbi
 
-
-for file in ${templates[@]};
-do
+for file in ${templates[@]}; do
 	cp wbi/backend/schemas/output/excel/json/${file} src/_data/workbooks/${file}
 done
 
-for file in ${instructions[@]};
-do
+for file in ${instructions[@]}; do
 	cp wbi/backend/schemas/source/excel/libs/${file} src/_data/workbooks/${file}
 	base=$(basename "${file}" .libsonnet)
-	jsonnet src/_data/workbooks/${file} > "src/_data/workbooks/${base}.json"
+	jsonnet src/_data/workbooks/${file} >"src/_data/workbooks/${base}.json"
 done
 
 rm -rf wbi
