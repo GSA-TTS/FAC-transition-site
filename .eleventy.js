ea7a4068ff72876038aa1f34abfd4bdf5d14bc4e
Matt Henry <hey@matthenry.fyi>
2023-09-26 14:39:12 -0400

    Just copy the whole assets folder over to the build dir

--- .eleventy.js
+++ .eleventy.js
@@ -1,40 +1,39 @@
 const yaml = require('js-yaml');
 
-const pluginRss = require("@11ty/eleventy-plugin-rss");
-const md = require("markdown-it")({
+const pluginRss = require('@11ty/eleventy-plugin-rss');
+const md = require('markdown-it')({
   html: false,
   breaks: true,
   linkify: true,
 });
 
-const hashCode = function(s) {
+const hashCode = function (s) {
   var hash = 0,
-    i, chr;
+    i,
+    chr;
   if (s.length === 0) return hash;
   for (i = 0; i < s.length; i++) {
     chr = s.charCodeAt(i);
-    hash = ((hash << 5) - hash) + chr;
+    hash = (hash << 5) - hash + chr;
     hash |= 0;
   }
-  return ((hash) >>> 0).toString(16);
+  return (hash >>> 0).toString(16);
 };
 
-module.exports = function(eleventyConfig) {
+module.exports = function (eleventyConfig) {
   eleventyConfig.addDataExtension('yaml', (contents) => yaml.load(contents));
-  eleventyConfig.addPassthroughCopy('assets/img');
-  eleventyConfig.addPassthroughCopy('assets/compliance');
-  eleventyConfig.addPassthroughCopy('assets/workbooks');
+  eleventyConfig.addPassthroughCopy('assets');
   eleventyConfig.addPassthroughCopy('favicon.ico');
   eleventyConfig.addPassthroughCopy('robots.txt');
 
   eleventyConfig.addPlugin(pluginRss);
-  eleventyConfig.addFilter("hashcode", (s) => hashCode(s));
-  eleventyConfig.addFilter("markdown", (markdownString) =>
+  eleventyConfig.addFilter('hashcode', (s) => hashCode(s));
+  eleventyConfig.addFilter('markdown', (markdownString) =>
     md.render(markdownString)
   );
 
   return {
-    markdownTemplateEngine: "njk",
+    markdownTemplateEngine: 'njk',
     dir: {
       input: 'src',
       includes: '_includes',
