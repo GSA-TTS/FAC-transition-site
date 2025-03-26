577d21d86540072a4beb537bbec47065746148d6
James Person <jperson1@umbc.edu>
2025-03-26 13:19:14 -0400

    Comments, remove unused frontmatter

--- src/_data/config.js
+++ src/_data/config.js
@@ -1,5 +1,5 @@
-const apiDataGovKey = "AGuVZDiVrItBRVCdtcGTIo4iaiwipPQZ3ymjkfOy";
+const apiDataGovKey = "AGuVZDiVrItBRVCdtcGTIo4iaiwipPQZ3ymjkfOy"; // This is embedded here on purpose. It's a public key.
 const baseUrl = process.env.BASEURL;
 
 exports.apiDataGovKey = apiDataGovKey;
-exports.baseUrl = typeof baseUrl !== 'undefined' ? baseUrl + '/' : '/';
+exports.baseUrl = typeof baseUrl !== "undefined" ? baseUrl + "/" : "/";
